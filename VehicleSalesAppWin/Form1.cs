4249c710cb55f85e23a2b39ede30695f8fefb3cd
Mous <mousahmed00@gmail.com>
2023-01-19 13:02:28 +0200

    Form ready , when button clicked pass the information to the list

--- VehicleSalesAppWin/Form1.cs
+++ VehicleSalesAppWin/Form1.cs
@@ -1,14 +1,17 @@
 
-using VehicleSalesStore;
-using VehicleSalesStore.Models;
+
+using CarClassLibrary;
 
 namespace VehicleSalesAppWin
 {
     public partial class Form1 : Form
     {
+       
+        Store myStore = new Store();
+        BindingSource carInventoryBindingSource = new BindingSource();
+        BindingSource cartBindingSource = new BindingSource();
 
-        BindingSource vehicleInventoryBindingSource = new BindingSource();
-        BindingSource cartBindigSource = new BindingSource();
+
         public Form1()
         {
             InitializeComponent();
@@ -27,13 +30,11 @@ namespace VehicleSalesAppWin
 
         private void btn_create_car_Click(object sender, EventArgs e)
         {
-
-            Vehicle vehicle = new Vehicle(txt_maker.Text,txt_model.Text,txt_year.Text,txt_price.Text);
-            MessageBox.Show("Vehicle details\nMaker: " + vehicle.maker + "\nModel: " + vehicle.model + "\nYear: " + vehicle.year + "\nPrice: " + vehicle.price);
-
-          /*  List<Vehicle> list = new List<Vehicle>();
-            ShowAndAdd.vehicleList.Add(vehicle);*/
-            
+            Car car = new Car(txt_maker.Text, txt_model.Text, txt_year.Text, decimal.Parse(txt_price.Text));
+           // string carDetails = $"Maker:{car.Maker}, Model:{car.Model}, Year:{car.Year}, Price:{car.Price}";
+            //MessageBox.Show(carDetails);
+            myStore.carList.Add(car);
+            carInventoryBindingSource.ResetBindings(false);
         }
 
         private void btn_add_to_cart_Click(object sender, EventArgs e)
@@ -73,6 +74,11 @@ namespace VehicleSalesAppWin
 
         private void Form1_Load(object sender, EventArgs e)
         {
+
+            carInventoryBindingSource.DataSource = myStore.carList;
+
+            lst_inventory.DataSource = carInventoryBindingSource;
+            lst_inventory.DisplayMember = "ToString";
 
         }
     }
